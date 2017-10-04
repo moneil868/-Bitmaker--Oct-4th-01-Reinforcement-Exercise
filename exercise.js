f7be5ea1be11272930a549b1d51c894a40e6cb9d
Marlon O'Neil <hello@marlononeil.com>
2017-10-04 16:04:30 -0400

    Completed exercise

---
+++ exercise.js
@@ -0,0 +1,6 @@
+var items = ["purchase domain", "install SSL certificate", "deploy site", "celebrate success"];
+  console.log('Todo:');
+for(var i = 0; i < items.length; i++){
+  count = i + 1
+  console.log(count + ': ' + items[i]);
+}
